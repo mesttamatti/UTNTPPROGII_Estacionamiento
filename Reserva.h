48235f4ee93da4899fad39b71f2e22b1c8978d27
Marina Esttamatti <105522615+mesttamatti@users.noreply.github.com>
2024-10-29 09:40:38 -0300

    Update Reserva.h
    REESRUCTURACION CORRECCION Y FINALIZACION. VER LUEGO SI LA AUTONUMERACION FUNCIONA BIEN

--- Reserva.h
+++ Reserva.h
@@ -1,30 +1,57 @@
 #pragma once
 #include "Fecha.h"
+int Reserva::_IDReserva =0; //inicializacion para que empiece a contar
 
 class Reserva{
 private:
-    int _IDReservas, _IDCliente, _IDEstacionamiento, _Dias;
+static int _IDReserva;
+    int _IDEmpleado;
+    int _IDCliente;
+    int _IDEstacionamiento; 
+    int _Dias;
     Fecha _Ingreso;
     float _Importe;
     bool _Estado;
 
 public:
     Reserva();
-    Reserva (int IDReservas, int IDClientes, int IDEstacionamiento, int Dias, Fecha Ingreso, float Importe, bool Estado);
-    int getIDReservas();
-    int getIDClientes();
-    int getIDEstacionamiento();
-    int getDias();
-    Fecha getFechaIngreso();
-    float getImporte();
-    bool getEstado();
-    void setIDReservas(int IDReservas);
-    void setIDClientes(int IDClientes);
-    void setIDEstacionamiento(int IDEstacionamiento);
-    void setDias(int Dias);
-    void setFechaI(Fecha Ingreso);
-    void setImporte(float Importe);
-    void setEstado (bool Estado);
+
+    int getIDReserva(){return _IDReserva;}
+    int getIDCliente(){return _IDCliente;}
+    int getIDEmpleado(){return _IDEmpleado;}
+    int getIDEstacionamiento(){return _IDEstacionamiento;}
+    int getDias(){return _Dias;}
+    Fecha getFechaIngreso(){return _Ingreso;}
+    float getImporte(){return _Importe;}
+    bool getEstado(){return _Estado;}
+
+    void setIDCliente(int cliente){_IDCliente = cliente;}
+    void setIDEmpleado(int empleado){_IDEmpleado = empleado;}
+    void setIDEstacionamiento(int estacionamiento){_IDEstacionamiento = estacionamiento;}
+    void setDias(int dias){_Dias = dias;}
+    void setFechaI(Fecha ingreso){_Ingreso= ingreso;}
+    void setImporte(float importe){_Importe= importe;}
+    void setEstado (bool estado){_Estado= estado;}
+
+    void cargar(int empleado, int estacionamiento){
+        _IDReserva++;
+        _IDEmpleado= empleado;
+        _IDEstacionamiento = estacionamiento;
+        cout<<"NUMERO DE IDENTIFICACION DE CLIENTE: "; cin>>_IDCliente;
+        cout<<"FECHA DE INGRESO: "; _Ingreso.cargar();
+        cout<<"CANTIDAD DIAS ABONADOS: "; cin>>_Dias;
+        cout<<"IMPORTE TOTAL: "; cin>>_Importe;
+        _estado=true;
+    }
+
+void mostrar(){
+    cout<<"NUMERO DE IDENTIFICACION DE RESERVA: "<<_IDReserva<<endl;
+    cout<<"EMPLEADO #: "<<_IDEmpleado<<" FECHA: "<<_Ingreso.mostrar();
+    cout<<"NUMERO DE ESTACIONAMIENTO: "<<_IDEstacionamiento<<endl;
+    cout<<"NUMERO DE CLIENTE: "<<_IDCliente<<endl;
+    cout<<"CANTIDAD DIAS ABONADOS: ";<<_Dias<<endl;
+    cout<<"IMPORTE TOTAL: "; cin>>_Importe;
+}
 
     std::string toCSV();
 };
