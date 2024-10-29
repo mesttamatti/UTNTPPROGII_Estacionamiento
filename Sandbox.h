849ccbdbd97568e0b2eb599d23547d0e66e31712
Marina Esttamatti <105522615+mesttamatti@users.noreply.github.com>
2024-10-29 14:20:45 -0300

    Create Sandbox.h
    para poder tener las ideas a la mano

---
+++ Sandbox.h
@@ -0,0 +1,29 @@
+//aqui se puede ir escribiendo pedazos de codigo, partes, dejar ideas, bocetos, etc
+/*
+Archivos: mejor hacer funcion que cargue los registros en un vector dinamico, entonces las busquedas se personalizan en funciones (evitar abrir y cerrar archivo a cada rato)
+
+Archivo movimientos: Escribir archivo solo los 15 ultimos
+Ultimo en agregarse es el primero
+
+logica:
+crear registro nuevo de movimiento
+
+abrir archivo
+int cantReg= archivo.contar reg() //verificar si es 15 o menor, aplicara diferente supongo(?
+crear vec din movimientos[cantReg];
+cargar el vector con los registros guardados
+
+archivo.vaciar();
+
+registro nuevo.escribir()
+hago un for(r=cantReg;r>0;r--){
+vector[r].escribir()
+}
+
+Solicitudes de reserva: abl 
+
+>>>>>>>>>faltan las funciones de busqueda, reciben el vector dinamico cargado con el archivo indicado
+>>>>>>>>>archivo reservas
+>>>>>>>>>archivo cliente
+
+*/
