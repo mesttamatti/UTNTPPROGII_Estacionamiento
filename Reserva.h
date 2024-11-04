df9bbef271e95434d04f4997f241889244f99ebf
Marina Esttamatti <105522615+mesttamatti@users.noreply.github.com>
2024-11-04 13:34:07 -0300

    Update Reserva.h
    completado las validaciones, se restringio el set empleado e importe. se completo el traer precio del estacionamiento via archivo

--- Reserva.h
+++ Reserva.h
@@ -28,25 +28,58 @@ public:
     float getImporte(){return _Importe;}
     bool getEstado(){return _Estado;}
 
-    void setIDCliente(int cliente){_IDCliente = cliente;}
-    void setIDEmpleado(int empleado){_IDEmpleado = empleado;}
-    void setIDEstacionamiento(int estacionamiento){_IDEstacionamiento = estacionamiento;}
+    void setIDCliente(int cliente){
+        ArchivoEstacionamiento archivCli;
+        while(archivCli.validarId(cliente)==false){
+            cout<<"ID NO VALIDO. REINTENTE: ";
+            cin>>cliente;
+            cout<<endl;
+        }
+        _IDCliente = cliente;}
+
+    void setIDEstacionamiento(int estacionamiento){
+        ArchivoEstacionamiento archivEst;
+        while(archivEst.validarId(estacionamiento)==false){
+            cout<<"ID NO VALIDO. REINTENTE: ";
+            cin>>estacionamiento;
+            cout<<endl;
+        }_IDEstacionamiento = estacionamiento;}
+
     void setDias(int dias){_Dias = dias;}
     void setFechaI(Fecha ingreso){_Ingreso= ingreso;}
-    void setImporte(float importe){_Importe= importe;}
     void setEstado (bool estado){_Estado= estado;}
 
     void cargar(int empleado, int estacionamiento){
+        ArchivoEstacionamiento archivEst;
+        ArchivoEstacionamiento archivCli;
+        
         _IDReserva++;
         _IDEmpleado= empleado;
+       
+        while(archivEst.validarId(estacionamiento)==false){
+            cout<<"ID NO VALIDO. REINTENTE: ";
+            cin>>estacionamiento;
+            cout<<endl;
+        }
         _IDEstacionamiento = estacionamiento;
+        
         cout<<"NUMERO DE IDENTIFICACION DE CLIENTE: "; cin>>_IDCliente;
+        while(archivCli.validarId(_IDCliente)==false){
+            cout<<"ID NO VALIDO. REINTENTE: ";
+            cin>>_IDCliente;
+            cout<<endl;
+        }
+        
         cout<<"FECHA DE INGRESO: "; _Ingreso.cargar();
         cout<<"CANTIDAD DIAS ABONADOS: "; cin>>_Dias;
+
         //abrir arch estacionamiento y traer valor por dia, multiplicarlo
-        //_Importe= estacionamiento.getPrecio(archivoestacionamiento.leerregistro(archivoestacionamiento.buscarporID(estacionamiento)))
+        Estacionamiento reg;
+        
+        _Importe= reg.getPrecio(archivEst.leerRegistro(archivEst.buscarporID(estacionamiento)));
         _Importe=_Importe*_Dias;
         cout<<"IMPORTE TOTAL: "; _Importe;
+        
         _estado=true;
     }
 
